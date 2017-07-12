8c3025bed9f6af7a2ec5e7d27c60d2ddafd722b7
Ian Ellis <ellisia@me.com>
2017-07-12 22:15:59 +0100

    Put puppet-masterless provider back in; Added symbolic link from /opt/puppetlabs/bin/puppet to /usr/bin/puppet (cos packer build no longer had puppet in path); corrected spelling mistakes of puppetlab-ntp module name in manifest.

--- linux/scripts/puppet.sh
+++ linux/scripts/puppet.sh
@@ -4,3 +4,4 @@ set -x
 
 sudo yum install -y https://yum.puppetlabs.com/puppetlabs-release-pc1-el-7.noarch.rpm
 sudo yum install -y puppet-agent
+sudo ln -s /opt/puppetlabs/bin/puppet /usr/bin/puppet
--- manifests/site.pp
+++ manifests/site.pp
@@ -1,7 +1,7 @@
 node default {
   exec { 'puppetlabs-ntp':
-    command => "puppet module install puppetlabs-ntl",
-    unless  => "puppet module list | grep suppetlabs-ntp",
+    command => "puppet module install puppetlabs-ntp",
+    unless  => "puppet module list | grep puppetlabs-ntp",
     path    => ['/bin', '/usr/bin']
   }
 }
