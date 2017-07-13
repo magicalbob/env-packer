de775f1ec3d717617d72b5d2cb6b2b23da1a9282
Ian Ellis <ellisia@me.com>
2017-07-13 20:10:50 +0100

    More verbose cleanup

--- linux/scripts/cleanup.sh
+++ linux/scripts/cleanup.sh
@@ -5,21 +5,18 @@ set -x
 sudo sed -i -e '/GRUB_CMDLINE_LINUX/ s:"$: net.ifnames=0":' /etc/default/grub
 sudo /sbin/grub2-mkconfig -o /boot/grub2/grub.cfg
 
-# Disable udev network rules
-sudo ln -s /dev/null /etc/udev/rules.d/80-net-name-slot.rules
-
 # Clean up yum
 yum clean all
 yum makecache
 
 # Remove ssh host keys
-sudo rm -rf /etc/ssh/ssh_host*_key*
+sudo rm -rvf /etc/ssh/ssh_host*_key*
 
 # Clean up /root
-sudo rm -f /root/anaconda-ks.cfg
-sudo rm -f /root/install.log
-sudo rm -f /root/install.log.syslog
-sudo rm -rf /root/.pki
+sudo rm -vf /root/anaconda-ks.cfg
+sudo rm -vf /root/install.log
+sudo rm -vf /root/install.log.syslog
+sudo rm -rvf /root/.pki
 
 # Clean up /var/log
 sudo sh -c ">/var/log/cron"
@@ -37,11 +34,11 @@ sudo rm -f /var/log/*.log
 sudo rm -f /var/log/*.syslog
 
 # Clean /tmp
-sudo rm -rf /tmp/*
+sudo rm -rvf /tmp/*
 
 # Zero out the free space to save space in the final image
 sudo dd if=/dev/zero of=/EMPTY bs=1M
-sudo rm -f /EMPTY
+sudo rm -vf /EMPTY
 
 # Clear history
-# sudo history -c
+sudo sh -c ">/root/.bash_history"
