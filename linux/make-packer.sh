8ec1bfc51c3353f3e8208e9a34ad46652e9b85c5
Ian Ellis <ellisia@me.com>
2017-07-22 23:00:07 +0100

    Updated make-packer.sh to take account of variable box name.

--- linux/make-packer.sh
+++ linux/make-packer.sh
@@ -21,10 +21,10 @@ packer build packer-template.json
 
 if [[ ${ARTIFACTORY_USERNAME} ]]
 then
-  CHECKSUM=$(md5sum packer_virtualbox-iso_virtualbox.box | awk '{ print $1 }')
+  CHECKSUM=$(md5sum ${OS_TYPE}-${MACHINE_SIZE}-vagrant.box | awk '{ print $1 }')
 
   curl --header "X-Checksum-MD5:${CHECKSUM}" \
        -u${ARTIFACTORY_USERNAME}:${ARTIFACTORY_PASSWORD} \
-       -T packer_virtualbox-iso_virtualbox.box \
-       "https://dev.ellisbs.co.uk/artifactory/vagrant-local/centos7_pipat;box_name=centos7_pipat;box_provider=virtualbox;box_version=${VERSION}"
+       -T ${OS_TYPE}-${MACHINE_SIZE}-vagrant.box \
+       "https://dev.ellisbs.co.uk/artifactory/vagrant-local/${OS_TYPE}-${MACHINE_SIZE}.box;box_name=${OS_TYPE}-${MACHINE_SIZE};box_provider=virtualbox;box_version=${VERSION}"
 fi
