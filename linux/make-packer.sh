96e32c134d950ac3ffa3965526b82a058effd02a
Ian Ellis <ellisia@me.com>
2017-07-23 00:11:15 +0100

    vagrant should have been virtualbox

--- linux/make-packer.sh
+++ linux/make-packer.sh
@@ -21,10 +21,10 @@ packer build packer-template.json
 
 if [[ ${ARTIFACTORY_USERNAME} ]]
 then
-  CHECKSUM=$(md5sum ${OS_TYPE}-${MACHINE_SIZE}-vagrant.box | awk '{ print $1 }')
+  CHECKSUM=$(md5sum ${OS_TYPE}-${MACHINE_SIZE}-virtualbox.box | awk '{ print $1 }')
 
   curl --header "X-Checksum-MD5:${CHECKSUM}" \
        -u${ARTIFACTORY_USERNAME}:${ARTIFACTORY_PASSWORD} \
-       -T ${OS_TYPE}-${MACHINE_SIZE}-vagrant.box \
+       -T ${OS_TYPE}-${MACHINE_SIZE}-virtualbox.box \
        "https://dev.ellisbs.co.uk/artifactory/vagrant-local/${OS_TYPE}-${MACHINE_SIZE}.box;box_name=${OS_TYPE}-${MACHINE_SIZE};box_provider=virtualbox;box_version=${VERSION}"
 fi
