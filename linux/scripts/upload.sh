a4b67dff9ed925515b9937ec846bdcca4a92ede6
Ian Ellis <ellisia@me.com>
2017-08-01 14:16:27 +0100

    Added environment variable to supply vcloud password to ovftool

--- linux/scripts/upload.sh
+++ linux/scripts/upload.sh
@@ -4,5 +4,5 @@ set -x
 
 echo "Uploading to vCloud"
 cd output-vmware-iso/
-ovftool --vCloudTemplate --acceptAllEulas --overwrite rhel-7.2-vmware.vmx "vcloud://$VCLOUD_USER@api.vcd.portal.skyscapecloud.com:443?org=$VCLOUD_ORG&vappTemplate=$VCLOUD_VAPP&catalog=$VCLOUD_CATALOG"
+echo $VCLOUD_PASSWORD|ovftool --vCloudTemplate --acceptAllEulas --overwrite centos7.vmx "vcloud://$VCLOUD_USER@api.vcd.portal.skyscapecloud.com:443?org=$VCLOUD_ORG&vappTemplate=$VCLOUD_VAPP&catalog=$VCLOUD_CATALOG"
 cd ..
