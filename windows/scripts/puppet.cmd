0c84735dddbfa64edc0c87ffbeaa8221874cb354
Ian Ellis <ellisia@me.com>
2017-08-06 20:48:26 +0100

    Copy windows manifest to production environment directory before running it, so that it is available when box is stood up.

--- windows/scripts/puppet.cmd
+++ windows/scripts/puppet.cmd
@@ -1,4 +1,5 @@
 puppet apply a:\puppetlabs-windows.pp
 puppet apply a:\choco.pp
 puppet apply a:\choco-config.pp
-puppet apply a:\windows.pp
+copy a:\windows.pp c:\ProgramData\PuppetLabs\code\environments\production\manifests\site.pp
+puppet apply c:\ProgramData\PuppetLabs\code\environments\production\manifests\site.pp
