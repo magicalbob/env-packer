6e2544d03e516cdaabd46bd82aa9fcf99da56eb7
Ian Ellis <ellisia@me.com>
2017-08-06 10:47:53 +0100

    Switched local build of vagrant micro back to dev branch of hieradata

--- linux/local-build-vagrant.micro.sh
+++ linux/local-build-vagrant.micro.sh
@@ -7,7 +7,7 @@ export SSH_USERNAME=root
 export SSH_PASSWORD=packer
 export MACHINE_SIZE=micro
 export CLOUD_TYPE=vagrant
-export BRANCH=lab
+export BRANCH=dev
 unset  ARTIFACTORY_USERNAME
 
 sh make-packer.sh | tee build.log
