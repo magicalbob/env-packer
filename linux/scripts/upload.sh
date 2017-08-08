2eafb3f53fed45744c1ca122c808d26c3c6f50d6
Ian Ellis <ellisia@me.com>
2017-08-08 21:00:53 +0100

    Use cot to replace network with one in the vcloud director environment

--- linux/scripts/upload.sh
+++ linux/scripts/upload.sh
@@ -3,6 +3,6 @@
 set -x
 
 echo "Uploading to vCloud"
-cd output-vmware-iso/
-echo $VCLOUD_PASSWORD|ovftool --vCloudTemplate --acceptAllEulas --overwrite centos7.vmx "vcloud://$VCLOUD_USER@api.vcd.portal.skyscapecloud.com:443?org=$VCLOUD_ORG&vappTemplate=$VCLOUD_VAPP&catalog=$VCLOUD_CATALOG"
-cd ..
+ovftool --vCloudTemplate --acceptAllEulas --overwrite output-vmware-iso/centos7.vmx tmp.ovf/centos7.ovf
+cot -f -v edit-hardware tmp.ovf/centos7/centos7.ovf -o tmp.ovf/centos7/centos7new.ovf -N ${VCLOUD_NET} --network-descriptions "VM Network"
+echo $VCLOUD_PASSWORD|ovftool --vCloudTemplate --acceptAllEulas --overwrite tmp.ovf/centos7/centos7new.ovf "vcloud://$VCLOUD_USER@api.vcd.portal.skyscapecloud.com:443?org=$VCLOUD_ORG&vappTemplate=$VCLOUD_VAPP&catalog=$VCLOUD_CATALOG"
